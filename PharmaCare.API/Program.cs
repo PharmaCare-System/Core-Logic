d1fb06f525730a049848c3143258418ac615ffee
eslams3dawi <eslamseadawy717@gmail.com>
2025-04-09 06:22:27 +0200

    Modify Registration of identity

--- PharmaCare.API/Program.cs
+++ PharmaCare.API/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PharmaCare.DAL.Database;
+using PharmaCare.DAL.Models;
 
 namespace PharmaCare.API
 {
@@ -21,7 +22,7 @@ namespace PharmaCare.API
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                     options.UseSqlServer(builder.Configuration.GetConnectionString("Server=.; Database = PharmaCare; Integrated Security = True; Trust Server Certificate = True")));
 
-            builder.Services.AddIdentity<ApplicationDbContext, IdentityRole>()
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             var app = builder.Build();
