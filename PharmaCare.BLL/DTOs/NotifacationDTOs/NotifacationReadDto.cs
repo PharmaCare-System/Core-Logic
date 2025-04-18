55a5dc2a7875eb4043c341a1e78b95c0c5458542
Moaaz Yasser <moazyasser070@gmail.com>
2025-04-19 01:39:50 +0200

    more edits for Eslam

--- PharmaCare.BLL/DTOs/NotifacationDTOs/NotifacationReadDto.cs
+++ PharmaCare.BLL/DTOs/NotifacationDTOs/NotifacationReadDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PharmaCare.DAL.Enums;
 using PharmaCare.DAL.Models;
 
 namespace PharmaCare.BLL.DTOs.NotifaciionDTOs
