5ecaf0d3870bd11685980d3d14b5928457d14c7e
eslams3dawi <eslamseadawy717@gmail.com>
2025-04-25 18:31:17 +0300

    Add: Introduced ProductService and related DTOs

---
+++ PharmaCare.BLL/DTOs/ProductDTOs/ProductUpdateDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaCare.BLL.DTOs.ProductDTOs
+{
+    public class ProductUpdateDTO
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public bool BulkAllowed { get; set; } = false;
+        public bool PrescriptionRequired { get; set; } = true;
+        public string ImageURL { get; set; }
+
+        public int InventoryId { get; set; }
+    }
+}
---
+++ PharmaCare.BLL/Services/ProductService/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using PharmaCare.BLL.DTOs.ProductDTOs;
+using PharmaCare.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaCare.BLL.Services.ProductService
+{
+    public interface IProductService
+    {
+        public Task AddAsync(ProductAddDTO productDTO);
+        public Task UpdateAsync(ProductUpdateDTO productDTO, int id);
+        public Task DeleteAsync(int id);
+        public Task<IEnumerable<ProductReadDTO>> GetAllAsync();
+        public Task<IEnumerable<ProductReadDTO>> GetAllByInventoryAsync(int inventoryId);
+        public Task<ProductReadDTO> GetAsyncById(int id);
+    }
+}
