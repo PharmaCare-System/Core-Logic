049a3f156afd8ca9dda5056f7d2d51e2a8815414
eslams3dawi <eslamseadawy717@gmail.com>
2025-04-25 03:44:32 +0300

    Feat: Add Prescription entity configuration

---
+++ PharmaCare.DAL/Configurations/PrescriptionConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using PharmaCare.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaCare.DAL.Configurations
+{
+    public class PrescriptionConfigurations : IEntityTypeConfiguration<Prescription>
+    {
+        public void Configure(EntityTypeBuilder<Prescription> builder)
+        {
+            builder.Property(p => p.Status)
+                   .HasConversion<string>()
+                   .HasMaxLength(10)
+                   .IsRequired(false);
+
+            builder.Property(p => p.UploadDate)
+                   .HasConversion<DateTime>()
+                   .IsRequired();
+        }
+    }
+}
