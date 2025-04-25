e9df52f25ed43f261e91dbdc99f837e70b5785fc
eslams3dawi <eslamseadawy717@gmail.com>
2025-04-25 03:44:18 +0300

    Chore: Remove old Customer and Pharmacist repositories

--- PharmaCare.DAL/Repository/Pharmacists/IPharmacistRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using PharmaCare.DAL.Repository.GenericRepository;
-using PharmaCare.DAL.Models;
-
-namespace PharmaCare.DAL.Repository.Pharmacists
-{
-    public interface IPharmacistRepository : IGenericRepository<Pharmacist>
-    {
-    }
-}
