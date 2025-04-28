fddab20f64a62dfe0f92873da05eb3faf69f49b1
eslams3dawi <eslamseadawy717@gmail.com>
2025-04-28 17:27:54 +0300

    feature: add customer and pharmacist address DTOs and customer address repository

---
+++ PharmaCare.DAL/Repository/CustomerAddressRepository/ICustomerAddressRepository.cs
@@ -0,0 +1,15 @@
+﻿using PharmaCare.DAL.Models.UserAddress;
+using PharmaCare.DAL.Repository.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaCare.DAL.Repository.CustomerAddressRepository
+{
+    public interface ICustomerAddressRepository : IGenericRepository<DAL.Models.UserAddress.CustomerAddress>
+    {
+
+    }
+}
