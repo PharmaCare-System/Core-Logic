a1c17b8f3584129ded52c9d4ce1639e1e0da12f9
eslams3dawi <eslamseadawy717@gmail.com>
2025-04-28 17:28:18 +0300

    Add Address logic

---
+++ PharmaCare.DAL/Repository/PharmacistAddress/PharmacistAddressRepository.cs
@@ -0,0 +1,18 @@
+﻿using PharmaCare.DAL.Database;
+using PharmaCare.DAL.Repository.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaCare.DAL.Repository.PharmacistAddress
+{
+    public class PharmacistAddressRepository : GenericRepository<DAL.Models.UserAddress.PharmacistAddress>, IPharmacistAddressRepository
+    {
+        public PharmacistAddressRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
