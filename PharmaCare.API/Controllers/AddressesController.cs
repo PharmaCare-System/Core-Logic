4a7d08d46e0afe5de556dd197838c08cafc8dfe0
eslams3dawi <eslamseadawy717@gmail.com>
2025-05-21 09:37:14 +0300

    Add endpoints images

--- PharmaCare.API/Controllers/AddressesController.cs
+++ PharmaCare.API/Controllers/AddressesController.cs
@@ -1,29 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using PharmaCare.BLL.Services.AddressService;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using PharmaCare.BLL.Services.AddressService;
 
-namespace PharmaCare.API.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AddressesController : ControllerBase
-    {
-        private readonly IAddressService _addressService;
+//namespace PharmaCare.API.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class AddressesController : ControllerBase
+//    {
+//        private readonly IAddressService _addressService;
        
-        public AddressesController(IAddressService addressService)
-        {
-            _addressService = addressService;
-        }
+//        public AddressesController(IAddressService addressService)
+//        {
+//            _addressService = addressService;
+//        }
 
-        [HttpGet("{customerId}")]
-        public async Task<IActionResult> GetCustomerAddressById(int customerId)
-        {
-            var customerAddress = _addressService.GetCustomerAsyncById(customerId);
-            if (customerAddress != null)
-                return Ok(customerAddress);
+//        [HttpGet("{customerId}")]
+//        public async Task<IActionResult> GetCustomerAddressById(int customerId)
+//        {
+//            var customerAddress = _addressService.GetCustomerAsyncById(customerId);
+//            if (customerAddress != null)
+//                return Ok(customerAddress);
 
-            return BadRequest();
-        }
+//            return BadRequest();
+//        }
 
-    }
-}
+//    }
+//}
